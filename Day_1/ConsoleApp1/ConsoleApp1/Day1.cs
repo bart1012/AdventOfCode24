bc37bf1cea3ee5d746770751c2240c06e33fbb36
bart <bielski.bart1012@gmail.com>
2024-12-26 18:08:43 +0000

    Complete Day 1 challenge

---
+++ Day_1/ConsoleApp1/ConsoleApp1/Day1.cs
@@ -0,0 +1,94 @@
+﻿namespace ConsoleApp1
+{
+    internal class Day1
+    {
+        //public static (List<int>, List<int>) ConvertToList(string path)
+        //{
+        //    List<int> leftList = new();
+        //    List<int> rightList = new();
+        //    bool leftListSwitch = true;
+        //    string input = File.ReadAllText(path);
+        //    int digits = CountDigits(input);
+
+        //    var modifiedInput = new string(input.Replace("\r\n", "").Replace(" ", "").Replace("\n", ""));
+
+        //    StringBuilder stringBuilder = new();
+
+        //    int intParse;
+
+        //    for (int i = 1; i <= digits * 2 * 49; i++)
+        //    {
+
+        //        stringBuilder.Append(modifiedInput[i - 1]);
+
+        //        if (i % digits == 0)
+        //        {
+        //            string completedstr = stringBuilder.ToString();
+        //            intParse = int.Parse(completedstr);
+        //            if (leftListSwitch) { leftList.Add(intParse); leftListSwitch = false; }
+        //            else { rightList.Add(intParse); leftListSwitch = true; }
+        //            stringBuilder.Clear();
+        //        }
+
+        //    }
+        //    return (leftList, rightList);
+        //}
+
+        public static void Solution_Day1_Part1(string path)
+        {
+            int result = 0;
+            List<int> leftList = new();
+            List<int> rightList = new();
+
+
+            foreach (var line in File.ReadLines(path))
+            {
+                string modifiedLine = line.Replace("/r", "").Replace("/n", "");
+                var splitString = modifiedLine.Split("   ");
+                int left = int.Parse(splitString[0]);
+                int right = int.Parse(splitString[1]);
+                leftList.Add(left);
+                rightList.Add(right);
+            }
+
+            leftList.Sort();
+            rightList.Sort();
+
+            for (int i = 0; i < leftList.Count; i++)
+            {
+                result += (Math.Max(leftList[i], rightList[i]) - Math.Min(leftList[i], rightList[i]));
+            }
+        }
+
+        public static void Solution_Day1_Part2(string path)
+        {
+            int result = 0;
+            List<int> leftList = new();
+            List<int> rightList = new();
+
+
+            foreach (var line in File.ReadLines(path))
+            {
+                string modifiedLine = line.Replace("/r", "").Replace("/n", "");
+                var splitString = modifiedLine.Split("   ");
+                int left = int.Parse(splitString[0]);
+                int right = int.Parse(splitString[1]);
+                leftList.Add(left);
+                rightList.Add(right);
+            }
+
+            leftList.Sort();
+            rightList.Sort();
+
+            for (int i = 0; i < leftList.Count; i++)
+            {
+                result += result += leftList[i] * (rightList.Where(x => x == leftList[i]).Count());
+
+            }
+        }
+
+
+
+    }
+}
+
---
+++ Day_1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1.ConvertToList("./input.txt");
+            //int result = Day1.FindDistance(lists.Item1, lists.Item2);
+            //Console.WriteLine(result);
+        }
+    }
+}
